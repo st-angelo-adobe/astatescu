64c6cf05fcd70ded8788e0f7a66fc65c295d8645
st-angelo-adobe <angelostatescu.adobe@gmail.com>
2024-08-12 16:48:17 +0300

    rnr js #1

--- blocks/rnr/rnr.js
+++ blocks/rnr/rnr.js
@@ -6,11 +6,16 @@ export default async function init(el) {
   const { createTag } = await import(`${miloLibs}/utils/utils.js`);
 
   const getRatingStar = (value) => {
-    const inputTag = createTag('input', { class: 'rating-star', value, name: 'rating' });
+    const inputTag = createTag('input', {
+      class: 'rating-star',
+      value,
+      name: 'rating',
+      type: 'radio',
+    });
     return inputTag;
   };
 
-  Array.from({ length: 12 }, (_, index) => index + 1).forEach((number) => {
+  Array.from({ length: 5 }, (_, index) => index + 1).forEach((number) => {
     const star = getRatingStar(number);
     el.append(star);
   });
